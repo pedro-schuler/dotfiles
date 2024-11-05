494fabe0c0f5260f7f2ccccd8142447287e6f2b6
Pedro Schuler <schuler.p.m@gmail.com>
2024-11-05 01:59:31 -0300

    Atualizado OhMyPosh e Adicionado suporte a arquivos LaTeX ao neovim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,109 +1,117 @@
 return {
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    config = function()
-        require("configs.mason-dap")
-    end,
-  },
-  
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-        "mfussenegger/nvim-dap",
-        "rcarriga/nvim-dap-ui",
+    {
+        "lervag/vimtex",
+        ft = {
+            "tex",
+            "bib",
+        },
     },
-    config = function()
-        require("configs.dap-python")
-    end,
-  },
 
-  {
-      "nvim-neotest/nvim-nio",
-  },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        config = function()
+            require "configs.mason-dap"
+        end,
+    },
 
-  {
-      "rcarriga/nvim-dap-ui",
-      dependencies = {
-          "mfussenegger/nvim-dap",
-          "nvim-neotest/nvim-nio",
-      },
-      config = function()
-          require("configs.dap-ui")
-      end,
-  },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            require "configs.dap-python"
+        end,
+    },
 
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-        require("configs.dap")
-    end,
-  },
+    {
+        "nvim-neotest/nvim-nio",
+    },
 
-  {
-    "rshkarin/mason-nvim-lint",
-    event = "VeryLazy",
-    dependencies = { "nvim-lint" },
-    config = function()
-        require("configs.mason-lint")
-    end,
-  },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            require "configs.dap-ui"
+        end,
+    },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-lspconfig" },
-    config = function()
-        require("configs.mason-lspconfig")
-    end,
-  },
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            require "configs.dap"
+        end,
+    },
 
-  {
-    "zapling/mason-conform.nvim",
-    event = "VeryLazy",
-    dependencies = { "conform.nvim" },
-    config = function()
-        require("configs.mason-conform")
-    end,
-  },
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require "configs.mason-lint"
+        end,
+    },
 
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("configs.lint")
-    end,
-  },
-  
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = require "configs.treesitter"
-  },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require "configs.mason-lspconfig"
+        end,
+    },
 
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require "configs.mason-conform"
+        end,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require "configs.lint"
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = require "configs.treesitter",
+    },
+
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- uncomment for format on save
+        opts = require "configs.conform",
+    },
+
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
 }
