be7795b2c036e8b8a3d41317c0552f089df7dd99
Pedro Schuler <schuler.p.m@gmail.com>
2025-01-28 23:52:54 -0300

    Limpa o arquivo de comentários desnecessários

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -3,7 +3,6 @@ return {
   event = 'VeryLazy',
   ft = { 'org' },
   config = function()
-    -- Setup orgmode
     require('orgmode').setup({
       org_agenda_files = '~/org/agenda/**/*',
       org_default_notes_file = '~/org/notes.org',
