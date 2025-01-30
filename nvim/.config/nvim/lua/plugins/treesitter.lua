96b07e1e4105895200c5d79d39ff743193327f65
Pedro Schuler <schuler.p.m@gmail.com>
2025-01-30 19:21:00 -0300

    Adiciona autoclose tags e suporte a Svelte

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,32 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			auto_install = true,
-			-- ensure_installed = { "lua", "javascript", "html" },
-			-- sync_install = false,
-			highlight = {
-				enable = true,
-				disable = { "latex", "org" },
-			},
-			indent = { enable = true },
-		})
-	end,
+      require("nvim-ts-autotag").setup({
+        opts = {
+          -- Defaults
+          enable_close = true,      -- Auto close tags
+          enable_rename = true,     -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
+      })
+
+      configs.setup({
+        auto_install = true,
+        -- ensure_installed = { "lua", "javascript", "html" },
+        -- sync_install = false,
+        highlight = {
+          enable = true,
+          disable = { "latex", "org" },
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+  },
 }
