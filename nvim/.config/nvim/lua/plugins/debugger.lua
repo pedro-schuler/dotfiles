ba99998dba5ad2bff4ccf5db999094191fbc9e54
Pedro Schuler <schuler.p.m@gmail.com>
2025-02-02 15:11:24 -0300

    Corrige a configuração do DAP, desabilita o Alpha.nvim e utiliza o Snacks no lugar

--- nvim/.config/nvim/lua/plugins/debugger.lua
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -1,38 +1,38 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
-			"mfussenegger/nvim-dap-python",
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-			-- Adapters
-			require("dap-python").setup("python")
+      -- Adapters
+      require("dap-python").setup("python")
 
-			-- Dapui Config
-			require("dapui").setup({})
+      -- Dapui Config
+      require("dapui").setup()
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			-- Keybindings
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint(), {})
-			vim.keymap.set("n", "<Leader>db", dap.set_breakpoint(), {})
-			vim.keymap.set("n", "<Leader>dc", dap.continue(), {})
-		end,
-	},
+      -- Keybindings
+      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<Leader>db", dap.set_breakpoint, {})
+      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end,
+  },
 }
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,19 +4,36 @@ return {
   lazy = false,
   opts = {
     animate = { enabled = false },
-    bigfile = { enabled = true },
+    bigfile = {
+      enabled = true,
+    },
     bufdelete = { enabled = false },
-    dashboard = { enabled = false },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
     debug = { enabled = false },
     dim = { enabled = false },
     explorer = { enabled = false },
     git = { enabled = false },
     gitbrowse = { enabled = false },
-    indent = { enabled = true },
-    input = { enabled = true },
+    indent = {
+      enabled = true,
+    },
+    input = {
+      enabled = true,
+    },
     layout = { enabled = false },
     lazygit = { enabled = false },
-    notifier = { enabled = true },
+    notifier = {
+      enabled = true,
+    },
     picker = { enabled = false },
     profiler = { enabled = false },
     quickfile = { enabled = false },
